25a3f4f40d9bfd77ec0ba8648a64832bae602bb5
abhi1301 <abhishek.kumar4444@gmail.com>
2017-07-11 03:39:23 +0530

    images are uploaded to image hosting site and links are generated

--- common/javascripts/uploadimage.js
+++ common/javascripts/uploadimage.js
@@ -23,29 +23,89 @@ $('#selectImage').on('change', function () {
   }
 
 });
-
+$('#uploadFile').on('click', function () {
+  $('.hideCroping').hide();
+  $('#image1Load').click();
+});
+$('.hideCroping').hide();
 // $('#image-cropper').cropit();
 // $imageCropper.cropit();
 $( document ).ready(function() {
     console.log( "ready!" );
     $('#image-cropper').cropit();
     $('#image-cropper2').cropit();
+    $('#image-cropper3').cropit();
+    $('#image-cropper4').cropit();
 
 });
 
-// $('#image-cropper').cropit(
-//     {
-//         onImageLoaded: function() {
-//             alert('loaded');
-//             var data = $("#image-cropper").cropit('imageSrc');
-//             console.log(data);
-//         }
-//     }
-// );
+$('#image-cropper').cropit(
+    {
+        onImageLoaded: function() {
+            var size = $('#image-cropper').cropit('imageSize');
+            console.log(true && true);
+            console.log((size.width >= 1024) && (size.height >= 1024));
+            console.log(size.height >= 1024);
+            if ((size.width >= 1024) && (size.height >= 1024) ){
+              console.log("yoyo");
+              $('.hideCroping').show();
+              var data = $("#image-cropper").cropit('imageSrc');
+              $("#image-cropper2").cropit('imageSrc', data);
+              $("#image-cropper3").cropit('imageSrc', data);
+              $("#image-cropper4").cropit('imageSrc', data);
+            }
+            else {
+              console.log("size not suitable");
+            }
+
+        }
+    }
+);
 
 // $("#image-cropper2").cropit('imageSrc',$("#image-cropper").cropit('imageSrc'));
 
-$('.download-btn').click(function() {
-  var imageData = $('#image-cropper').cropit('export');
-  window.open(imageData);
+$('#cropAll').click(function() {
+  // var imageData = $('#image-cropper').cropit('export');
+  // window.open(imageData);
+  // var imageData = $('#image-cropper').cropit('export');
+//   window.open($('#image-cropper').cropit('export', { type: 'image/jpeg',  originalSize: true }));
+//   window.open($('#image-cropper2').cropit('export', {
+//   type: 'image/jpeg',
+//   originalSize: true
+// }));
+//   window.open($('#image-cropper3').cropit('export', {
+//   type: 'image/jpeg',
+//   originalSize: true
+// }));
+//   window.open($('#image-cropper4').cropit('export'), {
+//   type: 'image/jpeg',
+//   originalSize: true
+// });
+  var images = {};
+  images.a = $('#image-cropper').cropit('export', { type: 'image/jpeg',  originalSize: false });
+  images.b = $('#image-cropper2').cropit('export', { type: 'image/jpeg',  originalSize: false });
+  images.c = $('#image-cropper3').cropit('export', { type: 'image/jpeg',  originalSize: false });
+  images.d = $('#image-cropper4').cropit('export', { type: 'image/jpeg',  originalSize: false });
+
+  // console.log(images);
+  // var jstring = new Object();
+  // jstring.name = 'abhishek';
+  // jstring.pass = 'abhi1301';
+  // console.log(jstring);
+  // console.log(JSON.stringify(jstring));
+
+  $.ajax({
+    url: '/uploaded',
+    type: 'POST',
+    dataType: "json",
+    data:JSON.stringify(images),
+    contentType: 'application/json',
+    processData: false,
+    success: function (data) {
+      console.log('uploaded successfully..');
+    },
+    error: function (e) {
+       console.log(e.message);
+     }
+  });
 });
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var formidable = require('formidable');
-var fs = require('fs');
+var bodyParser = require('body-parser');
+var cloudinary = require('cloudinary');
+// var formidable = require('formidable');
+// var fs = require('fs');
+app.use(bodyParser.urlencoded({
+    extended: true,
+    limit: '5mb'
+}));
+
+app.use(bodyParser.json({limit: '5mb'}));
 
 app.use(express.static(path.join(__dirname, 'common')));
 
@@ -10,25 +18,54 @@ app.get('/', function (request, response) {
   response.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
+cloudinary.config({
+  cloud_name: 'arkam1301',
+  api_key: '114711174829612',
+  api_secret: 'fGC0zhZM0lH-sm2o5_knNjpXWdQ'
+});
+
 app.post('/uploaded', function ( request, response) {
 
-  var form  = new formidable.IncomingForm();
+  urls = new Object();
+  // urls.a = 'a';
+  // urls.b = 'b';
+  // urls.c = 'c';
+  // console.log(urls);
+  var linkCount = 0;
+  var errorCount = 0;
+  for (i in request.body){
+    // console.log("i "+ i);
+    // urls.i =" i";
+    // console.log(request.body[i]);
+    cloudinary.uploader.upload(request.body[i], function(result) {
+    if (result.url){
+      console.log("link : "+result.url);
+      urls.i = result.url;
+      linkCount += 1;
+    }
+    else {
+      console.log("error");
+      errorCount += 1;
+      // response.send(result);
+    }
+    });
+  }
+  // while (true) {
+  //   if (errorCount > 0) {
+  //     console.log("error occured");
+  //     response.send('{"failure" : "Update failed", "status" : 205}');
+  //     break;
+  //   }
+  //   else if (linkCount == 4) {
+  //     console.log("success");
+  //     console.log(urls);
+  //     response.send('{"success" : "Updated Successfully", "status" : 200}');
+  //     break;
+  //   }
+  // }
+  // console.log(urls);
+  response.send('{"success" : "Updated Successfully", "status" : 200}');
 
-  form.uploadDir = path.join(__dirname,'/uploaded');
-
-  form.on('file', function(field, file){
-    fs.rename(file.path, path.join(form.uploadDir,file.name));
-  });
-
-  form.on('error', function (err) {
-    console.log('Error: '+ err);
-  });
-
-  form.on('end',function () {
-    response.end('success');
-  });
-
-  form.parse(request);
 });
 
 var uploadServer = app.listen(8081, function () {
